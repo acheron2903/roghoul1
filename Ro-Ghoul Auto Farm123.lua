c7a443890c6f0ae48a1d169515578e86436c4341
acheron2903 <caixedap1234511@gmail.com>
2024-05-25 08:59:02 +0700

    Update Ro-Ghoul Auto Farm123.lua

--- Ro-Ghoul Auto Farm123.lua
+++ Ro-Ghoul Auto Farm123.lua
@@ -32,7 +32,7 @@ local myData = loadstring(game:HttpGet("loadstring(game:HttpGetAsync("https://ra
         ["Eto Yoshimura"] = false,
         ["Koutarou Amon"] = false,
         ["Nishiki Nishio"] = false,
-	    ["Touka Kirishima"] = false
+	["Touka Kirishima"] = false
     },
     DistanceFromNpc = 5,
     DistanceFromBoss = 8,
@@ -48,7 +48,7 @@ local array = {
         ["Eto Yoshimura"] = 1250,
         ["Koutarou Amon"] = 750,
         ["Nishiki Nishio"] = 250,
-	    ["Touka Kirishima"] =250
+	["Touka Kirishima"] =250
     },
 
     npcs = {["Aogiri Members"] = "GhoulSpawns", Investigators = "CCGSpawns", Humans = "HumanSpawns"},
